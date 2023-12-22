fc22b3c49d6c8ad58e7449cf7028958143385870
Ivanow07_ <138402069+Ivanow07@users.noreply.github.com>
2023-12-22 23:29:41 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Installing Flask..."
+pip install Flask
+
+echo "Installing discord.py..."
+pip install discord.py
+
+# Add any additional dependencies you need
+# For example:
+# pip install requests
+# pip install numpy
+# ...
+
+echo "Installation complete."
